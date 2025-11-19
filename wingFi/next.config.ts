89be87c67c272a517ad015b7c4507f7dd77f228d
mbcse <mbcse50@gmail.com>
2025-11-19 14:22:31 -0300

    fix2

--- wingFi/next.config.ts
+++ wingFi/next.config.ts
@@ -11,7 +11,10 @@ const nextConfig: NextConfig = {
   // Exclude problematic packages from being processed by Next.js
   serverExternalPackages: ['pino', 'thread-stream'],
   
-  // Webpack config (using --webpack flag in build script)
+  // Add empty turbopack config to silence warnings when using webpack
+  turbopack: {},
+  
+  // Webpack config (using --webpack flag in dev and build scripts)
   webpack: (config, { isServer }) => {
     // Ignore test files and directories in node_modules
     config.module = config.module || {};
